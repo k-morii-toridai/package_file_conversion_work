4da3e3efddf5c55dafb8c4101f685593cabda82d
k-morii-toridai <morii1132829@gmail.com>
2024-01-28 12:52:49 +0900

    Updated script

--- df2poscar.py
+++ df2poscar.py
@@ -1,53 +1,51 @@
+""" This module is written for convert df_poscar_replaced_point to new POSCAR file. """
+
 import os
 
 
-# 元のPOSCARファイルから5行目までを抽出して、新しいPOSCARファイルに書き込む関数
-def df2poscar(df, original_file="./POSCAR", output_file="./gen_data/POSCAR"):
+def df2poscar(df, original_poscar_path="./POSCAR", generated_poscar_path="./gen_data/POSCAR"):
     """
-    Writing the DataFrame(:df_coords_fix) to a POSCAR file.
-    param1: df=df_coords_fix: DataFrame that has 'X', 'Y', 'Z' columns about coords.
+    Writing the DataFrame(:df_poscar_ion_replaced_point) to a POSCAR file.
+    param1: df=df_poscar_ion_replaced_point: DataFrame that has 'X', 'Y', 'Z' columns about coords.
     param2: original POSCAR file
     param3: generated POSCAR file
     """
-    
-    # df_coords_fixを文字列に変換
-    def df2str(df):
-        df_coords_fix_str = df[['X', 'Y', 'Z']].to_string(header=False, index=False, index_names=False)
-        return df_coords_fix_str
+    # POSCARファイルを読み込む
+    with open(original_poscar_path, 'r') as file:
+        lines = file.readlines()
 
-    
-    # df_coords_fixから元素種を文字列として抽出する関数
-    def return_species(df):
-        species_line = ' '.join(df['Species'].unique())
-        num_line = ' '.join([str(len(df[df['Species'] == specie])) for specie in df['Species'].unique()])
-        return species_line + '\n' + num_line
+    # 最初の5行を抽出
+    comment_scalingfactor_lattice_line = lines[:5]
 
-    
-    # 元のPOSCARファイルの5行目までを抽出し，新しいファイルに書き込む
-    def wirte_header2poscar():
-        # 最初の5行を抽出
-        with open(original_file, 'r') as infile:
-            lines = infile.readlines()[:5]
-        # 新しいPOSCARファイルに書き込む
-        with open(output_file, 'w') as outfile:
-            outfile.writelines(lines)
-    
-    
-    # 新しいPOSCARファイルに書き込んでいく
-    def write_species2poscar():
-        with open(output_file, 'a') as file:
-            # すでに存在するテキストファイルに元素種を追記
-            file.write(return_species(df) + '\n')
-            # 元素種まで書かれたファイルにDirectという文字をを追記
-            file.write('Direct\n')
-            # 直交座標を追記
-            file.write(df_coords_fix_str + '\n')
+    # df_poscar_ion_replaced_pointから元素種を文字列として抽出
+    species_line = '  ' + '   '.join(df['atom_symbol'].unique())
+    num_line = '   ' + '   '.join([str(len(df[df['atom_symbol'] == specie])) for specie in df['atom_symbol'].unique()])
+    ion_per_species_line = species_line + '\n' + num_line
 
+    # 構造情報が始まる行を特定し，抽出
+    for i, line in enumerate(lines):
+        if ('Direct' in line) or ('Cartesian' in line):
+            start_line = i
+    selective_dynamics_line = lines[start_line]
 
-    # 関数をcall
+    # df_poscar_ion_replaced_pointを文字列に変換
+    ion_positions_line = df[['x', 'y', 'z']].to_string(col_space=3, header=False, index=False, index_names=False)
+    # 列間のスペースを1つから3つに変更
+    ion_positions_line = ion_positions_line.replace(' ', '   ')
+    # 各行の先頭にスペースを追加
+    ion_positions_line = '\n'.join('  ' + line for line in ion_positions_line.split('\n'))
+
+    # ここから書き込みパート
+    # 生成ファイル出力先を確認
     None if os.path.exists("gen_data") else os.makedirs('gen_data')
-    df_coords_fix_str = df2str(df)
-    wirte_header2poscar()
-    write_species2poscar()
-    
-    print(f"{output_file} にクラスタ化後の内容が書き込まれました。")
+    # 新しいPOSCARファイルに書き込む
+    with open(generated_poscar_path, 'w') as outfile:
+        outfile.writelines(comment_scalingfactor_lattice_line)
+        # すでに存在するテキストファイルに元素種を追記
+        outfile.write(ion_per_species_line + '\n')
+        # 元素種まで書かれたファイルにDirectという文字をを追記
+        outfile.write(selective_dynamics_line)
+        # 直交座標を追記
+        outfile.write(ion_positions_line + '\n')
+
+    print(f"{generated_poscar_path} に多原子イオンを点置換した情報がPOSCARに書き込まれました．")
